89490fae4c8ff94303e478bc3be2a954385cf3e4
NAOMI MORIS <n.moris.23@abdn.ac.uk>
2024-04-03 19:49:46 +0000

    changed size of webchat

--- ca4/public/js/client.js
+++ ca4/public/js/client.js
@@ -1,12 +1,12 @@
-(function (){
+(function () {
     const webchat = document.querySelector(".webchat");
     const socket = io();
 
     let uname;
 
-    webchat.querySelector(".join-screen #join-user").addEventListener("click", function (){
+    webchat.querySelector(".join-screen #join-user").addEventListener("click", function () {
         let username = webchat.querySelector(".join-screen #username").value;
-        if(username.length == 0){
+        if (username.length == 0) {
             return;
         }
 
@@ -16,9 +16,9 @@
         webchat.querySelector(".chat-screen").classList.add("active");
     });
 
-    webchat.querySelector(".chat-screen #send-message").addEventListener("click", function(){
+    webchat.querySelector(".chat-screen #send-message").addEventListener("click", function () {
         let message = webchat.querySelector(".chat-screen #message-input").value;
-        if (message.length == 0){
+        if (message.length == 0) {
             return;
         }
 
@@ -27,29 +27,29 @@
             text: message
         });
 
-        socket.emit("chat",{
+        socket.emit("chat", {
             username: uname,
             text: message
         });
         webchat.querySelector(".chat-screen #message-input").value = "";
     });
 
-    webchat.querySelector(".chat-screen #exit-chat").addEventListener("click", function (){
+    webchat.querySelector(".chat-screen #exit-chat").addEventListener("click", function () {
         socket.emit("exitUser", uname);
         window.location.href = window.location.href;
     })
 
-    socket.on("update", function (update){
+    socket.on("update", function (update) {
         renderMessage("update", update);
     });
 
-    socket.on("chat", function (message){
+    socket.on("chat", function (message) {
         renderMessage("other", message);
     });
 
-    function renderMessage(type,message){
+    function renderMessage(type, message) {
         let messageContainer = webchat.querySelector(".chat-screen .messages");
-        if (type == "my"){
+        if (type == "my") {
             let x = document.createElement("div");
             x.setAttribute("class", "message my-message");
             x.innerHTML = `<div><div class="name">You<div class="text">${message.text}</div> </div> </div>`;
@@ -59,7 +59,7 @@
             x.setAttribute("class", "message other-message");
             x.innerHTML = `<div><div class="name">${message.username}<div class="text">${message.text}</div> </div> </div>`;
             messageContainer.appendChild(x);
-        } else if(type == "update"){
+        } else if (type == "update") {
             let x = document.createElement("div");
             x.setAttribute("class", "update");
             x.innerHTML = message;
