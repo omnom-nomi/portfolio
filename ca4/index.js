f2a8b6cdb84b2590fca2be8d73cddbeed4635549
NAOMI MORIS <n.moris.23@abdn.ac.uk>
2024-04-03 14:44:10 +0000

    created code for the client.js and index.js and added listeners for them

--- ca4/index.js
+++ ca4/index.js
@@ -1,40 +1,26 @@
 const express = require("express");
-const socket = require("socket.io");
+const path = require("path");
 
-// App setup
-const PORT = 5000;
 const app = express();
-const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
+const server = require("http").createServer(app);
 
-});
+app.use(express.static(path.join(__dirname+"/public")));
 
-// Static files
-app.use(express.static("public"));
+server.listen(5000);
 
-// Socket setup
-const io = socket(server);
-
-//we use a set to store users, sets objects are for unique values of any type
-const activeUsers = new Set();
+const io = require("socket.io")(server);
 
 io.on("connection", function (socket) {
-  console.log("Made socket connection");
-
-  socket.on("new user", function (data) {
-    socket.userId = data;
-    activeUsers.add(data);
-    //... is the the spread operator, adds to the set while retaining what was in there already
-    io.emit("new user", [...activeUsers]);
+  socket.on("newUser", function (username){
+    socket.broadcast.emit("update", username + "joined the webchat");
   });
 
-  socket.on("disconnect", function () {
-      activeUsers.delete(socket.userId);
-      io.emit("user disconnected", socket.userId);
-    });
- 
-    socket.on("chat message", function (data) {
-      io.emit("chat message", data);
+  socket.on("exitUser", function (username){
+    socket.broadcast.emit("update", username + "left the webchat");
+  });
+
+  socket.on("chat", function (message){
+    socket.broadcast.emit("chat", message);
   });
 
 });
