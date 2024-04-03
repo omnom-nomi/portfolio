654542b77ae3fe24e143ced16ccc8992c69b25a4
NAOMI MORIS <n.moris.23@abdn.ac.uk>
2024-04-03 18:02:45 +0000

    added a back to contact page button

--- ca4/index.js
+++ ca4/index.js
@@ -4,22 +4,22 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
-app.use(express.static(path.join(__dirname+"/public")));
+app.use(express.static(path.join(__dirname + "/public")));
 
 server.listen(5000);
 
 const io = require("socket.io")(server);
 
 io.on("connection", function (socket) {
-  socket.on("newUser", function (username){
+  socket.on("newUser", function (username) {
     socket.broadcast.emit("update", username + " joined the webchat");
   });
 
-  socket.on("exitUser", function (username){
+  socket.on("exitUser", function (username) {
     socket.broadcast.emit("update", username + " left the webchat");
   });
 
-  socket.on("chat", function (message){
+  socket.on("chat", function (message) {
     socket.broadcast.emit("chat", message);
   });
 
